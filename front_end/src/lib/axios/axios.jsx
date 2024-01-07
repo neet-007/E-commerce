bf32539ecff684594cfee817968a37a1ed2274d3
neet-007 <moayedeffect@gmail.com>
2024-01-07 18:43:04 +0300

    new branch

--- front_end/src/lib/axios/axios.jsx
+++ front_end/src/lib/axios/axios.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
+
 axios.defaults.withCredentials
+axios.defaults.credentials = 'same-origin';
 const api = axios.create({
     baseURL:'http://127.0.0.1:8000/api'
 }
